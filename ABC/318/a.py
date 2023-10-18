8a500d0f168a23f95f272ea776c01c31a67efb4d
KamiyamaKouki <92080227+KoukiFOL@users.noreply.github.com>
2023-10-19 08:55:49 +0900

    commit

--- ABC/318/a.py
+++ ABC/318/a.py
@@ -2,3 +2,10 @@ input_line = list(map(int, input().split(" ")))
 m = input_line[0]
 n = input_line[1]
 p = input_line[2]
+
+for i in range(1, n+1):
+    if M + i*p <= n:
+        print(i)
+    break
+    elif i ==n:
+        print(0)
