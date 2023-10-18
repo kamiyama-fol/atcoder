b29b000d3c1ae97b2eb33af738ad62f6d6ed5c29
kouki ueyama <kohki_ueyama@yahoo.co.jp>
2023-10-18 23:27:52 +0900

    commit

--- ABC/323/a.py
+++ ABC/323/a.py
@@ -2,6 +2,6 @@ s = list(map(int, input()))
 range = [2, 4, 6, 7, 8, 10, 12 , 14, 16 ]
 answer = "Yes"
 for i in range:
-    if s[i-1] % 2 != 0:
+    if s[i] % 2 != 0:
         answer = "No"
 print(answer)
